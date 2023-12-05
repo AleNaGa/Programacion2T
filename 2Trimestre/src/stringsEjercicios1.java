2798d5816beba62fa898c254c55c81d494ee00be
K1MP4 <alextiloup@gmail.com>
2023-12-05 20:40:02 +0100

    dv

---
+++ 2Trimestre/src/stringsEjercicios1.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+public class stringsEjercicios1 {
+    public static void main (String[]args){
+        Scanner scan = new Scanner(System.in);
+        /*
+        System.out.println("\nEjercicio1");
+        // Un String que contenga tu nombre y un mensaje de bienvenida.
+        String nombre = "Alejandro";
+        System.out.println("Bienvenido " + nombre);
+
+        System.out.println("\nEjercicio2");
+        //Ahora que te pida tu nombre
+        System.out.print("Introduce tu nombre: ");
+        nombre = scan.next();
+        System.out.println("Bienvenido " + nombre);
+
+        System.out.println("\nEjercicio3");
+        //Aplicación con String contraseña. 3 intentos
+        //Cuando aciertes no pedirá más la contraseña
+        String contrasena = "password";
+        String contrasena1 = "";
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Introduce la contraseña: ");
+            contrasena1 = scan.next();
+            if (contrasena.equalsIgnoreCase(contrasena1)){
+                break;
+                System.out.println("Contraseña correcta");
+            }
+        }
+
+
+        System.out.println("\nEjercicio4");
+        //Del String "La lluvia en Sevilla es una maravilla" cuenta las vocales
+        String lluvia = "La lluvia en Sevilla es una maravilla";
+        int cont = 0;
+        for (int i = 0; i < lluvia.length(); i++) {
+            char letra = lluvia.charAt(i);
+            switch (letra) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    cont++;
+                default:
+            }
+        }
+        System.out.println("hay " + cont + " vocales");
+
+        System.out.println("\nEjercicio5");
+        // reemplaza todas las a por e
+        String lluviaReplace = lluvia.replace('a', 'e');
+        System.out.println(lluviaReplace);
+
+        System.out.println("\nEjercicio6");
+        // Dada una cadena mostrar cantidades de vocales.
+        System.out.print("Introduce una cadena: ");
+        String cad = scan.next();
+        int cont2 = 0;
+        for (int i = 0; i < cad.length(); i++) {
+            char letra = cad.charAt(i);
+            switch (letra) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    cont2++;
+                default:
+            }
+        }
+        System.out.println("hay " + cont2 + " vocales");
+
+        System.out.println("\nEjercicio7");
+        // Invertir una cadena
+        System.out.print("Introduce una cadena para invertir");
+        String cadena = scan.next();
+        String cadenaInv = "";
+        for (int i = 0; i < cadena.length(); i++) {
+            char almac = cadena.charAt(i);
+            cadenaInv = almac + cadenaInv;
+        }
+        System.out.println(cadenaInv);
+
+        System.out.println("\nEjercicio8");
+        // Una cadena y un caracter cuenta cuantas veces se repite.
+        System.out.print("Introduce una cadena: ");
+        String cadena8 = scan.nextLine();
+        System.out.print("Introduce un caracter: ");
+        String caractStr = scan.nextLine();
+        char caract = caractStr.charAt(0);
+        System.out.println(caract);
+        int cont8=0;
+        for (int i = 0; i < cadena8.length(); i++) {
+            char guardado8 = cadena8.charAt(i);
+            if( guardado8 == caract){
+                cont8++;
+            }
+        }
+        System.out.println(cont8);
+*/
+        System.out.println("\nEjercicio9");
+        // pasa ASCII a char
+        System.out.print("Introduce un número y verás su ASCII char: ");
+        int ascii = scan.nextInt();
+        char caracter = (char) ascii;
+        System.out.println(caracter);
+
+        System.out.println("\nEjercicio10");
+        // pasa ASCII a char
+        System.out.print("Introduce un número y verás su ASCII char: ");
+
+        ascii = scan.nextInt();
+        caracter = (char) ascii;
+        System.out.println(caracter);
+
+    }
+}
